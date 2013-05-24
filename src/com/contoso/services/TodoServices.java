268c43e8527b6919c4519c4dbbeaa56bd8ed9bb7
Will Smythe <wismythe@microsoft.com>
2013-05-24 17:16:06 -0400

    #MTA-3 #resolve Found the problem. Fixing

--- src/com/contoso/services/TodoServices.java
+++ src/com/contoso/services/TodoServices.java
@@ -47,9 +47,8 @@ public class TodoServices extends HttpServlet {
 	private TodoItem[] getTestItems() {
 		List items = new ArrayList();
 
-		items.add(new TodoItem("Get deployment with ANT to work"));		
-		items.add(new TodoItem("Get AWS deployment to work v2"));
-		items.add(new TodoItem("Deliver paper"));
+		items.add(new TodoItem("Get tickets for the game"));
+		items.add(new TodoItem("Mail package"));
 		items.add(new TodoItem("Pick up milk"));
 		items.add(new TodoItem("Mow the lawn"));
 		
