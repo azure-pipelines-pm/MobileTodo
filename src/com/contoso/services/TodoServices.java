83495f656efe1f09e4022bb56f0500564c0be307
Will Smythe <wismythe@microsoft.com>
2013-12-16 12:33:36 -0500

    Maven fixes

--- src/com/contoso/services/TodoServices.java
+++ src/com/contoso/services/TodoServices.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +17,7 @@ import com.google.gson.Gson;
 /**
  * Servlet implementation class TodoServices
  */
-
+@WebServlet("/TodoServices")
 public class TodoServices extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
