5836c1fdb13e7e3d5aa138454188264ee4803b0f
Will Smythe <wismythe@microsoft.com>
2014-01-06 20:13:38 -0500

    Change to blue

--- src/com/contoso/services/TodoServices.java
+++ src/com/contoso/services/TodoServices.java
@@ -1,16 +1,12 @@
-/* a simple change */
+/* Todo service */
 package com.contoso.services;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 import com.google.gson.Gson;
 
@@ -26,7 +22,6 @@ public class TodoServices extends HttpServlet {
      */
     public TodoServices() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -36,6 +31,7 @@ public class TodoServices extends HttpServlet {
 		Gson gson = new Gson();
                 TodoList todoList = new TodoList();
                 todoList.name = "Personal";
+                todoList.color = "blue";
                 todoList.items = getTestItems();
 
 		String json = gson.toJson(todoList);  
@@ -60,8 +56,9 @@ public class TodoServices extends HttpServlet {
 	private static class TodoList {
 
 		public String name;
-		public TodoItem[] items;
-
+		public String color;
+        public TodoItem[] items;
+        
 	}
 	
 	private static class TodoItem {
