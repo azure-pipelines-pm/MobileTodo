389ee9ea299395d7f50adc6682d63a911eb0690e
jamieleecool@hotmail.com <jamieleecool@hotmail.com>
2014-01-16 15:43:17 -0500

    fixup

--- src/com/contoso/services/TodoServices.java
+++ src/com/contoso/services/TodoServices.java
@@ -49,7 +49,7 @@ public class TodoServices extends HttpServlet {
 		items.add(new TodoItem("Get tickets for the game"));
 		items.add(new TodoItem("Mail package"));
 		items.add(new TodoItem("Feed the cows"));
-		items.add(new TodoItem("Support Windows"));
+		items.add(new TodoItem("Pickup the kids"));
 		
 		
 		return (TodoItem[])items.toArray(new TodoItem[items.size()]);
